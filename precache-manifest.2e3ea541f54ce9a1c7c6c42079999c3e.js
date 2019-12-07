f06d8610530b19d299a1a362f9f923881e0c8192
Gustavo Oliveira <gusttavo@live.com>
2019-12-06 23:35:15 -0300

    Updates

---
+++ precache-manifest.2e3ea541f54ce9a1c7c6c42079999c3e.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6c87b567ffa8438835192f8992ef351b",
+    "url": "/quiz/index.html"
+  },
+  {
+    "revision": "55f404b7f33298de5382",
+    "url": "/quiz/static/js/2.36124ffa.chunk.js"
+  },
+  {
+    "revision": "10c70b901226a363633a",
+    "url": "/quiz/static/js/main.9da974dc.chunk.js"
+  },
+  {
+    "revision": "8c609a62bdad736df464",
+    "url": "/quiz/static/js/runtime-main.d80cb9e3.js"
+  }
+]);
